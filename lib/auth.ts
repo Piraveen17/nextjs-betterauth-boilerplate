bd467a229fa5cab5860d9af010126beea1568178
Piraveen17 <Piraveen1727@gmail.com>
2025-12-04 12:12:13 +0530

    Updated some file structure

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,10 +12,6 @@ export const auth = betterAuth({
   appName: "Better Auth Kit",
   database: mongodbAdapter(db),
   socialProviders: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    },
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    domains: ["lh3.googleusercontent.com"], // add any other external domains here
+  },
 };
 
 export default nextConfig;
